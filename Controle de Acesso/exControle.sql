0a6f0c9a1dc455d79c8a2bd50fdd2e3c24ef22aa
AtlasGab <emilygsa@gmail.com>
2025-07-21 22:25:29 -0300

    Update exControle.sql

--- Controle de Acesso/exControle.sql
+++ Controle de Acesso/exControle.sql
@@ -21,25 +21,25 @@ create table Usuario (
 );
 
 create table Servidor (
-	id_servidor int primary key,
+	id_usuario int primary key,
     
     matricula varchar(50) unique not null,
     
-    foreign key(id_servidor) references Usuario(id)
+    foreign key(id_usuario) references Usuario(id)
 );
 
 create table Externo (
-	id_externo int primary key,
-    matricula_supervisor int not null, -- Relação de supervisão
+	id_usuario int primary key,
+    matricula_supervisor varchar(50) not null, -- Relação de supervisão
     
     cpf varchar(11) unique not null,
     
-    foreign key(id_externo) references Usuario(id),
+    foreign key(id_usuario) references Usuario(id),
     foreign key(matricula_supervisor) references Servidor(matricula)
 );
 
 -- Relacionamentos --
-create table Permissao_da_Sala (
+create table Permissao_Sala (
 	id_sala int,
     id_usuario int,
     horario time not null,
@@ -52,7 +52,7 @@ create table Permissao_da_Sala (
 create table Usuario_Possui_Tag (
 	id_tag int,
     id_usuario int,
-    horario time not null,
+    horario datetime not null,
     primary key(id_tag, id_usuario, horario),
     
     foreign key(id_tag) references Tag(id),
@@ -63,7 +63,7 @@ create table Usuario_Tenta_Acessar (
 	id_tag int,
     id_sala int,
     id_usuario int,
-    horario time not null,
+    horario datetime not null,
     permitido boolean,
     primary key(id_usuario, id_tag, id_sala, horario),
     
@@ -71,34 +71,4 @@ create table Usuario_Tenta_Acessar (
     foreign key(id_sala) references Sala(id),
     foreign key(id_usuario) references Usuario(id)
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
