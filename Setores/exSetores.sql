1e0255b5b983fd9488872f57fc187a53eecbdcdb
AtlasGab <emilygsa@gmail.com>
2025-07-23 23:35:01 -0300

    Update exSetores.sql

--- Setores/exSetores.sql
+++ Setores/exSetores.sql
@@ -3,12 +3,12 @@ create database Setores;
 use Setores;
 
 create table Setor (
-	codigo int primary key,
+    codigo int primary key,
     nome varchar(50) not null
 );
 
 create table Empregado (
-	cpf varchar(11) primary key,
+    cpf varchar(11) primary key,
     nome varchar(80) not null,
     
     codigo_setor int not null, -- Relação lotado
@@ -16,27 +16,27 @@ create table Empregado (
 );
 
 create table Engenheiro (
-	cpf varchar(11) primary key,
+    cpf varchar(11) primary key,
     numero_de_registro int unique not null,
     
     foreign key(cpf) references Empregado(cpf)
 );
 
 create table Programador (
-	cpf varchar(11) primary key,
+    cpf varchar(11) primary key,
     especialidade varchar(70) not null,
     
     foreign key(cpf) references Empregado(cpf)
 );
 
 create table Projeto (
-	codigo int primary key,
+    codigo int primary key,
     nome varchar(100) not null
 );
 
 create table Engenheiro_Coordena_Projeto (
-	codigo_projeto int primary key,
-	registro_engenheiro int unique,
+    codigo_projeto int primary key,
+    registro_engenheiro int unique,
     data_coordenacao date,
     
     foreign key(codigo_projeto) references Projeto(codigo),
@@ -44,7 +44,7 @@ create table Engenheiro_Coordena_Projeto (
 );
 
 create table Programador_Participa_Projeto (
-	cpf_programador varchar(11),
+    cpf_programador varchar(11),
     codigo_projeto int,
     primary key(cpf_programador, codigo_projeto),
     
@@ -55,17 +55,17 @@ create table Programador_Participa_Projeto (
 );
 
 create table Linguagem_de_Programacao (
-	nome varchar(20) primary key,
+    nome varchar(20) primary key,
     multiplataforma boolean,
     aberta boolean
     
 );
 
 create table Programador_Domina_Linguagem (
-	cpf_programador varchar(11),
+    cpf_programador varchar(11),
     linguagem varchar(20),
     primary key(cpf_programador, linguagem),
     
     foreign key(cpf_programador) references Programador(cpf),
     foreign key(linguagem) references Linguagem_de_Programacao(nome)
-);
+); -- não tem como colocar que o programador saiba no mínimo uma linguagem com o conhecimento atual da disciplina, pesquisar futuramente.
